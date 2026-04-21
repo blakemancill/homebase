edab57c77a1682a4b97e768cbd71a48597247c4a
Blake Mancill <blakemancill9@gmail.com>
2026-04-21 15:52:12 -0400

    improved navbar on small screens by adding a hamburger toggle

--- src/templates/partials/navbar.rs
+++ src/templates/partials/navbar.rs
@@ -1,8 +1,29 @@
-use maud::{Markup, html};
+use maud::{Markup, html, PreEscaped};
 
 pub fn render_navbar(current_path: &str) -> Markup {
     html! {
-        aside #sidebar .menu {
+        style {
+            r#"
+                @media (max-width: 768px) {
+                    .is-hidden-narrow { display: none; }
+                }
+            "#
+        }
+        button .is-hidden-tablet.m-3 type="button"
+            _="on click toggle .is-hidden-narrow on #sidebar"
+        {
+            (PreEscaped(
+                r#"
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                    width="24" height="24">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" /></svg>
+                "#
+            ))
+        }
+
+        aside #sidebar .menu.is-hidden-narrow {
             div .menu {
                  ul .menu-list {
                     (nav_link("/", "Home", current_path))
