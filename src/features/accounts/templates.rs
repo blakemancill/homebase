a8126707adcb0ddfe1b7883bb643eeaf18bfc403
Blake Mancill <blakemancill9@gmail.com>
2026-05-12 18:15:10 -0400

    fleshed out accounts dashboard page

--- src/features/accounts/templates.rs
+++ src/features/accounts/templates.rs
@@ -2,6 +2,22 @@ use maud::{html, Markup};
 
 pub(crate) fn render_account_dashboard() -> Markup {
     html! {
-        p { "This is the account dashboard." }
+        div .container.is-fluid {
+            div .level {
+                div .level-left { h1 .title { "Accounts" } }
+                div .level-right {
+                    button .button.is-primary { "Add New Account" }
+                }
+            }
+            table .table.mx-auto {
+                thead {
+                    th { "Name" }
+                    th { "Bank" }
+                    th { "Opening Balance" }
+                    th { "Opening Date" }
+                    th { "Creation Date" }
+                }
+            }
+        }
     }
 }
